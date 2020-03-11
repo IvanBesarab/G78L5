e6f0637671933a6941164a1c81036716a7c9ee05
Ivan Besarab <IvanVBesarab@gmail.com>
2020-03-11 20:07:31 +0200

    Initial Commit

---
+++ G78L5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  G78L5
+//
+//  Created by Ivan Vasilevich on 11.03.2020.
+//  Copyright © 2020 ivan.besarab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
